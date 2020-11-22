69256b291493d09c09d3a7e43c20f32afa68ff8b
engjuyeon <engjeong43@gmail.com>
2020-11-23 01:10:43 +0900

    current weather edit

--- app/src/main/java/com/example/how_about_camping/WeatherActivity.java
+++ app/src/main/java/com/example/how_about_camping/WeatherActivity.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -24,6 +23,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -37,16 +37,9 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 public class WeatherActivity extends AppCompatActivity {
 
     Handler handler = new Handler();
@@ -55,32 +48,44 @@ public class WeatherActivity extends AppCompatActivity {
     final String TAG = "GPS";
 
     LocationManager locationManager;
-    TextView tvCity, tvLatitude, tvLongitude, tvWeather, tvDate, tvTemp_c, tvTemp_f;
+    TextView humidityId, current_rain,tvCity, tvLatitude, tvLongitude, tvdescription, weatherMain, tvDate, tvTemp_c, temp_high, temp_low, feels_temp;
     RelativeLayout vProgressLayer;
     RequestQueue requestQueue;
 
     public static final Integer MY_PERMISSIONS_REQUEST_LOCATION = 0x5;
-    ImageView weatherIcon;
+    ImageView weatherIconR;
+
     Bitmap bitmap;
+    private String rain_1h;
+    private String rain_3h;
 
-    //ConstraintLayout weatherBackground = (ConstraintLayout) findViewById(R.id.weatherBackground);
-
+    ConstraintLayout background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_main);
 
+        background = (ConstraintLayout)findViewById(R.id.weatherBackground);
+        weatherIconR = (ImageView) findViewById(R.id.weatherIcon);
+        current_rain = (TextView) findViewById(R.id.textViewPrecipitation);
         tvCity = (TextView) findViewById(R.id.tvCity);
         // tvLatitude = (TextView) findViewById(R.id.tvLatitude);
         //  tvLongitude = (TextView) findViewById(R.id.tvLongitude);
-        tvWeather = (TextView) findViewById(R.id.temp_high);
+        tvdescription = (TextView) findViewById(R.id.weather_description);
         tvDate = (TextView) findViewById(R.id.tvDate);
         tvTemp_c = (TextView) findViewById(R.id.tvTemp_c);
         vProgressLayer = (RelativeLayout) findViewById(R.id.progressLayer);
-
+        temp_high = (TextView)findViewById(R.id.temp_high);
+        temp_low = (TextView)findViewById(R.id.temp_low);
+        feels_temp = (TextView)findViewById(R.id.feels_temp);
+        weatherMain = (TextView)findViewById(R.id.weather_main);
+        humidityId = (TextView)findViewById(R.id.humidity);
         locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+
         ImageButton backBtn = (ImageButton) findViewById(R.id.weatherBackBtn);
+
+        setBackgroundByTime();
 
         // Register the listener with the Location Manager to receive location updates
         if (ContextCompat.checkSelfPermission(this,
@@ -128,13 +133,13 @@ public class WeatherActivity extends AppCompatActivity {
 
         int time = Integer.parseInt(hourText);
         if (time >= 0 && time < 6) {
-            //weatherBackground.setBackgroundResource(R.drawable.sunny_night_background);
+            background.setBackground(ContextCompat.getDrawable(this, R.drawable.sunny_night_background));
         } else if (time >= 6 && time < 15) {
-          //  weatherBackground.setBackgroundResource(R.drawable.sunny_afternoon_background);
+            background.setBackground(ContextCompat.getDrawable(this, R.drawable.sunny_afternoon_background));
         } else if (time >= 15 && time < 20) {
-          //  weatherBackground.setBackgroundResource(R.drawable.sunny_sunset_background);
+            background.setBackground(ContextCompat.getDrawable(this, R.drawable.sunny_sunset_background));
         } else if (time >= 20 && time < 24) {
-          //  weatherBackground.setBackgroundResource(R.drawable.sunny_night_background);
+            background.setBackground(ContextCompat.getDrawable(this, R.drawable.sunny_night_background));
         }
     }
 
@@ -209,18 +214,16 @@ public class WeatherActivity extends AppCompatActivity {
     public void fetchLocationData(Location myLocation) {
         requestQueue = Volley.newRequestQueue(this);
 
-        Log.d(TAG, "got new location update");
+        Log.d(TAG, "새로운 위치가 업데이트되었습니다.");
 
         lat = Double.toString(myLocation.getLatitude());
         lon = Double.toString(myLocation.getLongitude());
         Log.d(TAG, "DAta: " + myLocation);
 
 
-        //String url = "api.openweathermap.org/data/2.5/forecast?lat=" + lat +"&lon="+lon+"&appid=8367e9646913ff43229d761791043b73=kr";
-      //  String url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&lang=zh_cn&appid=8367e9646913ff43229d761791043b73";
-        //  String url = "http://api.openweathermap.org/data/2.5/weather?appid=8367e9646913ff43229d761791043b73&units=metric&id=1835848&lang=kr";
-     //   url += "&lat=" + String.valueOf(lat) + "&lon=" + String.valueOf(lon);
-        String url = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
+         String url = "https://api.openweathermap.org/data/2.5/weather?appid=8367e9646913ff43229d761791043b73&units=metric&id=1835848&lang=kr";
+         url += "&lat=" + String.valueOf(lat) + "&lon=" + String.valueOf(lon);
+      //  String url = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
       //  url += "&lat="+String.valueOf(lat)+"&lon="+String.valueOf(lon);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
@@ -231,24 +234,115 @@ public class WeatherActivity extends AppCompatActivity {
                             String locationTemp = response.getJSONObject("main").getString("temp");
                             String country = response.getJSONObject("sys").getString("country");
                             String place = response.getString("name") + ", " + country;
+                            String temp_highR = response.getJSONObject("main").getString("temp_max");
+                            String temp_lowR = response.getJSONObject("main").getString("temp_min");
+                            String feels_tempR = response.getJSONObject("main").getString("feels_like");
+                            String humidityR = response.getJSONObject("main").getString("humidity");
                             String locationWeather = response.getJSONArray("weather").getJSONObject(0).getString("main");
-                            final String weatherIconUrl = response.getJSONArray("weather").getJSONObject(0).getString("icon");
+                            String weatherIconLoad = response.getJSONArray("weather").getJSONObject(0).getString("icon");
+                            String weatherDescription = response.getJSONArray("weather").getJSONObject(0).getString("description");
                             String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
 
-                            tvWeather.setText(locationWeather);
-                            tvTemp_c.setText(locationTemp + " \u2103");
-                            Double fahrenheit = Double.valueOf(locationTemp) * 1.8 + 32;
+                            tvdescription.setText(weatherDescription);
+                            weatherMain.setText(locationWeather);
+                            tvTemp_c.setText(locationTemp + "\u2103");
                             //   tvLatitude.setText(lat);
                             // tvLongitude.setText(lon);
                             tvDate.setText(currentDateTimeString);
                             tvCity.setText(place);
                             vProgressLayer.setVisibility(View.GONE);
+                            temp_high.setText("최고온도"+" "+temp_highR+"\u2103");
+                            temp_low.setText("최저온도"+" "+temp_lowR+"\u2103");
+                            feels_temp.setText(feels_tempR+"\u2103");
+                            humidityId.setText(humidityR + "%");
+
+                            // weatherIconR.setIma
+
+                            switch(weatherIconLoad){
+                                case "01d":
+                                    weatherIconR.setImageResource(R.drawable.icon_01d);
+                                    break;
+                                case "01n":
+                                    weatherIconR.setImageResource(R.drawable.icon_01n);
+                                    break;
+                                case "02d":
+                                    weatherIconR.setImageResource(R.drawable.icon_02d);
+                                    break;
+                                case "02n":
+                                    weatherIconR.setImageResource(R.drawable.icon_02n);
+                                    break;
+                                case "03d":
+                                    weatherIconR.setImageResource(R.drawable.icon_03d);
+                                    break;
+                                case "03n":
+                                    weatherIconR.setImageResource(R.drawable.icon_03n);
+                                    break;
+                                case "04d":
+                                    weatherIconR.setImageResource(R.drawable.icon_04d);
+                                    break;
+                                case "04n":
+                                    weatherIconR.setImageResource(R.drawable.icon_04n);
+                                    break;
+                                case "09d":
+                                    weatherIconR.setImageResource(R.drawable.icon_09d);
+                                    break;
+                                case "09n":
+                                    weatherIconR.setImageResource(R.drawable.icon_09n);
+                                    break;
+                                case "10d":
+                                    weatherIconR.setImageResource(R.drawable.icon_10d);
+                                    break;
+                                case "10n":
+                                    weatherIconR.setImageResource(R.drawable.icon_10n);
+                                    break;
+                                case "11d":
+                                    weatherIconR.setImageResource(R.drawable.icon_11d);
+                                    break;
+                                case "11n":
+                                    weatherIconR.setImageResource(R.drawable.icon_11n);
+                                    break;
+                                case "13d":
+                                    weatherIconR.setImageResource(R.drawable.icon_13d);
+                                    break;
+                                case "13n":
+                                    weatherIconR.setImageResource(R.drawable.icon_13n);
+                                    break;
+                                case "50d":
+                                    weatherIconR.setImageResource(R.drawable.icon_50d);
+                                    break;
+                                case "50n":
+                                    weatherIconR.setImageResource(R.drawable.icon_50n);
+                                    break;
+                                default:
+                                    break;
+                            }
+
+                            //강우량
+                            if(response.has("rain")){
+                                JSONObject rain_object = response.getJSONObject("rain");
+                                if(rain_object.has("1h")){
+                                    rain_1h = rain_object.getString("1h");
+                                    rain_1h = String.valueOf(Math.round(Double.valueOf(rain_1h)*10));
+                                    current_rain.setText(rain_1h + "mm");
+
+                                }
+                                else if(rain_object.has("3h")){
+                                    rain_3h = rain_object.getString("3h");
+                                    rain_3h = String.valueOf(Math.round(Double.valueOf(rain_3h)*10));
+                                    current_rain.setText(rain_3h + "mm");
+                                }
+                                else {
+                                    current_rain.setText("0"+ "mm");
+                                }
+                            }
+                            else {
+                                current_rain.setText("0"+ "mm");
+                            }
 
 
-                            weatherIcon = findViewById(R.id.weatherIcon);
+                            //날씨 아이콘
 
-                           // setBackgroundByTime();
-
+                        /*
 
                             Thread uThread = new Thread() {
 
@@ -303,7 +397,7 @@ public class WeatherActivity extends AppCompatActivity {
 
                             }
 
-
+*/
                         } catch (JSONException e) {
                             //some exception handler code.
                             Log.d(TAG, "error occured: " + e);
@@ -325,20 +419,10 @@ public class WeatherActivity extends AppCompatActivity {
 
     }
 
-    public static int getResId(String resName, Class<?> c) {
-        try {
-            Field idField = c.getDeclaredField(resName);
-            return idField.getInt(idField);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
-        }
-    }
-
     public void showSettingsAlert() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setTitle("GPS is not Enabled!");
-        alertDialog.setMessage("Do you want to turn on GPS?");
+        alertDialog.setTitle("GPS에 연결되지 않았습니다.");
+        alertDialog.setMessage("GPS를 활성화하시겠습니까?");
 
         alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
